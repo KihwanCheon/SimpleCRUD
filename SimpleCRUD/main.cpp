11ee27956f7d149293f9b129fbd6fabe98a097e1
KihwanCheon <gujjy97@gmail.com>
2018-02-17 01:57:14 +0900

    first commit

---
+++ SimpleCRUD/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "sqlite3.h"
+
+int main() {
+
+
+
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << sqlite3_version << std::endl;
+    return 0;
+}
---
+++ SimpleCRUDTest/test_main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by NHNEnt on 2018. 2. 16..
+//
+#include <iostream>
+#include "gtest/gtest.h"
+
+int main()
+{
+
+    std::cout << "hihi" << std::endl;
+    return 0;
+}
+
